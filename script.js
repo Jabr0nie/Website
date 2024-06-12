874bf6173ae7bda82f852ffa4338f37fc0bf68ec
Nyke Finance <167595945+Jabr0nie@users.noreply.github.com>
2024-06-12 14:43:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ function myFunction() {
     var Logo = document.getElementById("logo");
     if (x.style.display === "block") {
       x.style.display = "none";
-      Logo.style.display = "block";
-
     } else {
       x.style.display = "block";
-      Logo.style.display = "none";
     }
   }
               // Setting getblock node as HTTP provider
